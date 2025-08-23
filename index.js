aff1fe1c189d7f6789a3aaaba7f6f9a3e7adbe22
Ardhi Lukianto <ardhi@lukianto.com>
2025-08-23 23:41:17 +0700

    Updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 async function factory (pkgName) {
   const me = this
 
-  return class MasohiCodec extends this.lib.BajoPlugin {
+  return class MasohiCodec extends this.lib.Plugin {
     constructor () {
       super(pkgName, me.app)
       this.alias = 'codec'
