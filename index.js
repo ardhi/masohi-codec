fb8946e667b0cf95896db7b9e0637bcd42367663
Ardhi Lukianto <ardhi@lukianto.com>
2025-09-29 19:52:42 +0700

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+/**
+ * Plugin factory
+ *
+ * @param {string} pkgName - NPM package name
+ * @returns {class}
+ */
 async function factory (pkgName) {
   const me = this
 
+  /**
+   * MasohiCodec class
+   *
+   * @class
+   */
   class MasohiCodec extends this.app.pluginClass.base {
     static alias = 'codec'
     static dependencies = ['masohi']
